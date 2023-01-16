4c98801b8898e1db4ba3a004e5d5dda3618c6bbc
Ariel Castro Neves <ariel.neves@planejamento.rj.gov.br.local>
2023-01-16 10:06:07 -0300

    tooltip e dropdown

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -1,19 +1,25 @@
-import { EventoService } from './services/evento.service';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
+// Componentes da aplicação
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Services da aplicação
+import { EventoService } from './services/evento.service';
+
+// Angular Components
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // ngx-bootstrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { DateTimeFormatPipe } from './helpers/date-time-format.pipe';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
     declarations: [
@@ -29,7 +35,9 @@ import { DateTimeFormatPipe } from './helpers/date-time-format.pipe';
         HttpClientModule,
         BrowserAnimationsModule,
         CollapseModule.forRoot(),
-        FormsModule
+        FormsModule,
+        TooltipModule,
+        BsDropdownModule.forRoot(),
     ],
     providers: [EventoService],
     bootstrap: [AppComponent],
