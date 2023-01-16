e8786a71d8ab252b4a1c6632c6c8f89585ae377c
Ariel Castro Neves <ariel.neves@planejamento.rj.gov.br.local>
2023-01-16 17:33:57 -0300

    referencia bootswatch e coponente título atualizando

--- frontend/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ frontend/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
     @Input() titulo: string | undefined;
+    @Input() iconClass = 'fa fa-user';
+    @Input() subtitulo = 'Desde 2021';
+    @Input() botaoListar = false;
 
     constructor() {}
 
