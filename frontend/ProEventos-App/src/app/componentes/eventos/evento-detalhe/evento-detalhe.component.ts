1514617d8013406a2b06564655196f12408af743
acastron22 <arielcastron22@hotmail.com>
2023-01-18 22:43:25 -0300

    formulário com validação

--- frontend/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
+++ frontend/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,51 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import {
-    FormBuilder,
-    FormControl,
-    FormGroup,
-    Validators,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-evento-detalhe',
-    templateUrl: './evento-detalhe.component.html',
-    styleUrls: ['./evento-detalhe.component.scss'],
+  selector: 'app-evento-detalhe',
+  templateUrl: './evento-detalhe.component.html',
+  styleUrls: ['./evento-detalhe.component.scss'],
 })
 export class EventoDetalheComponent implements OnInit {
-    form: FormGroup = this.formBuilder.group({});
-    get f(): any {
-        return this.form.controls;
-    }
+  form: FormGroup = this.formBuilder.group({});
+  get f(): any {
+    return this.form.controls;
+  }
 
-    constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-    ngOnInit(): void {
-        this.validation();
-    }
+  ngOnInit(): void {
+    this.validation();
+  }
 
-    public validation(): void {
-        this.form = this.formBuilder.group({
-            tema: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(4),
-                    Validators.maxLength(50),
-                ],
-            ],
-            local: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(3),
-                    Validators.maxLength(30),
-                ],
-            ],
-            dataEvento: ['', [Validators.required]],
-            qtdPessoas: ['', [Validators.required, Validators.min(1) ,Validators.max(120000)]],
-            telefone: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-            email: ['', [Validators.required, Validators.email]],
-            imageUrl: ['', [Validators.required]],
-        });
-    }
+  public validation(): void {
+    this.form = this.formBuilder.group({
+      tema: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ],
+      ],
+      local: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+      dataEvento: ['', [Validators.required]],
+      qtdPessoas: [
+        '',
+        [Validators.required, Validators.min(1), Validators.max(120000)],
+      ],
+      telefone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]],
+      imageUrl: ['', [Validators.required]],
+    });
+  }
+
+  resetForm(): void {
+    this.form.reset();
+    this.router.navigate(['/eventos/lista']);
+  }
 }
