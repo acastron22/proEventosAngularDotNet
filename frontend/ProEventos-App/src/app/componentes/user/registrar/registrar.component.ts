8d9d20eb33ba7800519eed8454329ba89690864d
Ariel Castro Neves <ariel.neves@planejamento.rj.gov.br.local>
2023-01-19 12:45:28 -0300

    validação da senha e formulário de registro

--- frontend/ProEventos-App/src/app/componentes/user/registrar/registrar.component.ts
+++ frontend/ProEventos-App/src/app/componentes/user/registrar/registrar.component.ts
@@ -1,15 +1,55 @@
+import { ValidatorFields } from './../../../helpers/validator-fields';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrls: ['./registrar.component.scss']
+  styleUrls: ['./registrar.component.scss'],
 })
 export class RegistrarComponent implements OnInit {
+  form: FormGroup = this.formBuilder.group({});
+  checkbox: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get registro(): any {
+    return this.form.controls;
   }
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.validacaoRegistro();
+  }
+
+
+  validacaoRegistro(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorFields.MustMatch('senha', 'confirmaSenha')
+    };
+
+    this.form = this.formBuilder.group(
+      {
+        primeiroNome: ['', [Validators.required, Validators.maxLength(25)]],
+        ultimoNome: ['', [Validators.required, Validators.maxLength(30)]],
+        email: ['', [Validators.required, Validators.email]],
+        usuario: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(15),
+          ],
+        ],
+        senha: ['', [Validators.required, Validators.minLength(8)]],
+        confirmaSenha: ['', [Validators.required]],
+      },
+      formOptions
+    );
+  }
 }
---
+++ frontend/ProEventos-App/src/app/helpers/validator-fields.spec.ts
@@ -0,0 +1,7 @@
+import { ValidatorFields } from './validator-fields';
+
+describe('ValidatorFields', () => {
+  it('should create an instance', () => {
+    expect(new ValidatorFields()).toBeTruthy();
+  });
+});
