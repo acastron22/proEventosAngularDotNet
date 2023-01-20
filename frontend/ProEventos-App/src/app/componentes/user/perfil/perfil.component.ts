577700d108885b3874b4f89cee05244ee1c437b5
acastron22 <arielcastron22@hotmail.com>
2023-01-20 12:22:08 -0300

    formulário de perfil finalizado

--- frontend/ProEventos-App/src/app/componentes/user/perfil/perfil.component.ts
+++ frontend/ProEventos-App/src/app/componentes/user/perfil/perfil.component.ts
@@ -16,6 +16,16 @@ import { ValidatorFields } from 'src/app/helpers/validator-fields';
 export class PerfilComponent implements OnInit {
   form: FormGroup = this.formBuilder.group({});
 
+  fromsfields: string[] = [
+    'Bacharel',
+    'Ariel',
+    'Castro Neves',
+    'ariel@ariel',
+    '9130343536',
+    'Participante',
+    'Ariel é um estudante',
+  ];
+
   get registro(): any {
     return this.form.controls;
   }
@@ -32,23 +42,15 @@ export class PerfilComponent implements OnInit {
 
     this.form = this.formBuilder.group(
       {
-        titulo: ['', [Validators.required]],
-        primeiroNome: ['', [Validators.required, Validators.maxLength(25)]],
-        ultimoNome: ['', [Validators.required, Validators.maxLength(30)]],
-        email: ['', [Validators.required, Validators.email]],
-        telefone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-        descricao: ['', [Validators.required]],
-        usuario: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(15),
-          ],
-        ],
-        senha: ['', [Validators.required, Validators.minLength(8)]],
-        confirmaSenha: ['', [Validators.required]],
-        termo: ['', [Validators.required, Validators.pattern('true')]],
+        titulo: [this.fromsfields[0], [Validators.required]],
+        primeiroNome: [this.fromsfields[1], [Validators.required, Validators.maxLength(25)]],
+        ultimoNome: [this.fromsfields[2], [Validators.required, Validators.maxLength(30)]],
+        email: [this.fromsfields[3], [Validators.required, Validators.email]],
+        telefone: [this.fromsfields[4], [Validators.required, Validators.pattern('^[0-9]*$')]],
+        funcao: [this.fromsfields[5]],
+        descricao: [this.fromsfields[6], [Validators.required]],
+        senha: ['', [Validators.minLength(8)]],
+        confirmaSenha: ['', [Validators.minLength(8)]],
       },
       formOptions
     );
