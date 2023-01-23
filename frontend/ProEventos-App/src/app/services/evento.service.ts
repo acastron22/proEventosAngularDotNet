ceb3c1ac299eba315e63842aa02c6f24701583b9
Ariel Castro Neves <ariel.neves@planejamento.rj.gov.br.local>
2023-01-23 14:46:56 -0300

    métodos de put delete e post adicionados ao service

--- frontend/ProEventos-App/src/app/services/evento.service.ts
+++ frontend/ProEventos-App/src/app/services/evento.service.ts
@@ -5,17 +5,26 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class EventoService {
-    baseUrl = 'https:localhost:5001/api/evento';
+  baseUrl = 'https:localhost:5001/api/evento';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getEvento(): Observable<IEvento[]> {
-        return this.http.get<IEvento[]>(this.baseUrl);
-    }
-    getEventosByTema(tema: string): Observable<IEvento> {
-        return this.http.get<IEvento>(`${this.baseUrl}/${tema}/tema`);
-    }
-    getEventoById(id: number): Observable<IEvento> {
-        return this.http.get<IEvento>(`${this.baseUrl}/${id}`);
-    }
+  getEvento(): Observable<IEvento[]> {
+    return this.http.get<IEvento[]>(this.baseUrl);
+  }
+  getEventosByTema(tema: string): Observable<IEvento> {
+    return this.http.get<IEvento>(`${this.baseUrl}/${tema}/tema`);
+  }
+  getEventoById(id: number): Observable<IEvento> {
+    return this.http.get<IEvento>(`${this.baseUrl}/${id}`);
+  }
+  postEvento(evento: IEvento): Observable<IEvento> {
+    return this.http.post<IEvento>(this.baseUrl, evento);
+  }
+  putEvento(id: number, evento: IEvento): Observable<IEvento> {
+    return this.http.put<IEvento>(`${this.baseUrl}/${id}`, evento);
+  }
+  deleteEvento(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.baseUrl}/${id}`);
+  }
 }
